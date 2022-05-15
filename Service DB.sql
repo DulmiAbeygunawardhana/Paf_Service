e4083ac7ceeaaaff3106ae6c36a6a2aa220499eb
Dulmi Himaya <89574455+IT20141288@users.noreply.github.com>
2022-05-15 22:46:59 +0530

    Service DB

---
+++ Service DB.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM service.service
+LIMIT 0, 1000
+
+-- Date: 2022-05-15 22:45
+*/
+INSERT INTO `` (`notificationID`,`topic`,`description`,`date`) VALUES (1034,'Bill','Bill Price = 2000','2020/05/25');
+INSERT INTO `` (`notificationID`,`topic`,`description`,`date`) VALUES (1035,'Management','Bill price','2020/05/10');
+INSERT INTO `` (`notificationID`,`topic`,`description`,`date`) VALUES (1036,'Bill','Bill price = 3000','2020/06/20');
